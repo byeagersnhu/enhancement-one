36cfcb2af941f113ffeebeb30b8cc1b59dcc0b14
Brandon Yeager <brandon.yeager@snhu.edu>
2026-04-18 11:27:21 -0600

    Remove sensitive environment files from tracking

--- aac-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    googleMapsApiKey: 'AIzaSyBx-lZ2vMkp21lrHmZ5QjrbkpPRuXKpxDI'
-};
--- aac-frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    googleMapsApiKey: 'AIzaSyBx-lZ2vMkp21lrHmZ5QjrbkpPRuXKpxDI'
-};
